e8a9645daee4687c4580f8f03e4b7fc929947df3
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2023-05-23 19:10:47 +0200

    PEP 8 style validation

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -25,15 +25,15 @@ def minOperations(n):
     """
     if n <= 0:
         return 0
-    
+
     operations = 0
-    
+
     for i in range(2, int(math.sqrt(n)) + 1):
         while n % i == 0:
-            operations += i 
+            operations += i
             n = n // i
 
     if n > 1:
         operations += n
-    
+
     return operations
